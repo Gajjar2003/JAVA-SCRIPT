3da9a1b7947e06c5715ef2d28249898840a2e158
JENIL1108 <jenilgajjar1108@gmail.com>
2026-06-29 17:05:14 +0530

    all

---
+++ React/Lect (27-06)/firstapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './conponets/Home';
+
+function App() {
+  return (
+    <>
+    <h1>Well come to react</h1>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ React/Lect (27-06)/myviteapp/src/conponets/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>Well come To React Vite</h2>
+    </div>
+  )
+}
+
+export default Home
